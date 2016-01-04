f59b54fa27bb4db00fa77a0d274f4cb82eb98365
imbavirus <jvdwpsp@gmail.com>
2016-01-04 10:39:00 +0200

    Travis

--- src/server/scripts/Commands/cs_ticket.cpp
+++ src/server/scripts/Commands/cs_ticket.cpp
@@ -381,7 +381,7 @@ bool ticket_commandscript::HandleTicketCreateCommand(ChatHandler* handler, char 
 		return false;
 	
 	Player* plr = handler->GetSession() ? handler->GetSession()->GetPlayer() : nullptr;
-	Ticket* ticket;
+	Ticket* ticket = new Ticket;
 	G3D::Vector3 position;
 	position = G3D::Vector3(plr->GetPositionX(), plr->GetPositionY(), plr->GetPositionZ());
 	ticket->SetPosition(plr->GetMapId(), position);
