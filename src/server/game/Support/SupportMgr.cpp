18c56740307124a3bf66a8acd81d69bb65ac0607
imbavirus <jvdwpsp@gmail.com>
2016-01-04 19:17:29 +0200

    Travis

--- src/server/game/Support/SupportMgr.cpp
+++ src/server/game/Support/SupportMgr.cpp
@@ -21,9 +21,9 @@
 
 inline time_t GetAge(uint64 t) { return (time(nullptr) - t) / DAY; }
 
-Ticket::Ticket() : _id(0), _mapId(0), _createTime(0), _facing(0.0f) { }
+Ticket::Ticket() : _id(0), _mapId(0), _facing(0.0f), _createTime(0){ }
 
-Ticket::Ticket(Player* player) : _id(0), _mapId(0), _createTime(time(nullptr)), _facing(0.0f)
+Ticket::Ticket(Player* player) : _id(0), _mapId(0), _facing(0.0f), _createTime(time(nullptr))
 {
     _playerGuid = player->GetGUID();
 	_id = sSupportMgr->GenerateTicketId();
